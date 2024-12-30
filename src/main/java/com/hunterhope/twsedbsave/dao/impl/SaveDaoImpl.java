dde4c72dd198f30c55cfcf498ee15ab6f0fae5df
hunterhope <hunterhope@outlook.com>
2024-12-30 23:14:06 +0800

    修改底層實作丟出的SQL例外都是java.sql的SQLException

--- src/main/java/com/hunterhope/twsedbsave/dao/impl/SaveDaoImpl.java
+++ src/main/java/com/hunterhope/twsedbsave/dao/impl/SaveDaoImpl.java
@@ -25,55 +25,75 @@ public class SaveDaoImpl implements SaveDao {
     }
 
     @Override
-    public int[] save(String tableName, List<StockEveryDayInfo> data) {
-        String sql = "INSERT INTO %s VALUES(?,?,?,?,?,?,?);";
-        int[] rowEffect = jdbcTemplate.batchUpdate(String.format(sql, tableName),
-                new BatchPreparedStatementSetter() {
-            @Override
-            public void setValues(PreparedStatement ps, int i) throws SQLException {
-                StockEveryDayInfo info = data.get(i);
-                ps.setString(1, info.getDate());
-                ps.setString(2, info.getVolume());
-                ps.setString(3, info.getOpen());
-                ps.setString(4, info.getHight());
-                ps.setString(5, info.getLow());
-                ps.setString(6, info.getClose());
-                ps.setString(7, info.getPriceDif());
-            }
+    public int[] save(String tableName, List<StockEveryDayInfo> data) throws SQLException {
+        try {
+            String sql = "INSERT INTO %s VALUES(?,?,?,?,?,?,?);";
+            int[] rowEffect = jdbcTemplate.batchUpdate(String.format(sql, tableName),
+                    new BatchPreparedStatementSetter() {
+                @Override
+                public void setValues(PreparedStatement ps, int i) throws SQLException {
+                    StockEveryDayInfo info = data.get(i);
+                    ps.setString(1, info.getDate());
+                    ps.setString(2, info.getVolume());
+                    ps.setString(3, info.getOpen());
+                    ps.setString(4, info.getHight());
+                    ps.setString(5, info.getLow());
+                    ps.setString(6, info.getClose());
+                    ps.setString(7, info.getPriceDif());
+                }
 
-            @Override
-            public int getBatchSize() {
-                return data.size();
-            }
+                @Override
+                public int getBatchSize() {
+                    return data.size();
+                }
 
-        });
+            });
 
-        return rowEffect;
+            return rowEffect;
+        } catch (Exception ex) {
+            throw new SQLException(ex);
+        }
     }
 
     @Override
-    public void createTable(String tableName) {
-        String sql = "CREATE TABLE IF NOT EXISTS %s (date TEXT,volume TEXT NOT NULL,open TEXT NOT NULL,hight TEXT NOT NULL,low TEXT NOT NULL,close TEXT NOT NULL,price_dif TEXT NOT NULL,PRIMARY KEY(date))";
-        jdbcTemplate.execute(String.format(sql, tableName));
+    public void createTable(String tableName) throws SQLException {
+        try {
+            String sql = "CREATE TABLE IF NOT EXISTS %s (date TEXT,volume TEXT NOT NULL,open TEXT NOT NULL,hight TEXT NOT NULL,low TEXT NOT NULL,close TEXT NOT NULL,price_dif TEXT NOT NULL,PRIMARY KEY(date))";
+            jdbcTemplate.execute(String.format(sql, tableName));
+        } catch (Exception ex) {
+            throw new SQLException(ex);
+        }
     }
 
     @Override
-    public String queryLastDate(String tableName) {
-        String sql="SELECT date FROM %s ORDER BY date LIMIT 1;";
-        return jdbcTemplate.queryForObject(String.format(sql, tableName), String.class);
+    public String queryLastDate(String tableName) throws SQLException {
+        try {
+            String sql = "SELECT date FROM %s ORDER BY date LIMIT 1;";
+            return jdbcTemplate.queryForObject(String.format(sql, tableName), String.class);
+        } catch (Exception ex) {
+            throw new SQLException(ex);
+        }
     }
 
     @Override
-    public String queryLatestDate(String tableName) {
-        String sql="SELECT date FROM %s ORDER BY date DESC LIMIT 1;";
-        return jdbcTemplate.queryForObject(String.format(sql, tableName), String.class);
+    public String queryLatestDate(String tableName) throws SQLException {
+        try {
+            String sql = "SELECT date FROM %s ORDER BY date DESC LIMIT 1;";
+            return jdbcTemplate.queryForObject(String.format(sql, tableName), String.class);
+        } catch (Exception ex) {
+            throw new SQLException(ex);
+        }
     }
 
     @Override
     public List<String> queryDates(String tableName, String yymmdd) throws SQLException {
-        String endYYMMDD=new StringBuffer(yymmdd).replace(yymmdd.length()-2, yymmdd.length(), "31").toString();
-        String sql="SELECT date FROM %s WHERE date >= '%s' AND date <= '%s';";
-        return jdbcTemplate.queryForList(String.format(sql, tableName,yymmdd,endYYMMDD), String.class);
+        try{
+        String endYYMMDD = new StringBuffer(yymmdd).replace(yymmdd.length() - 2, yymmdd.length(), "31").toString();
+        String sql = "SELECT date FROM %s WHERE date >= '%s' AND date <= '%s';";
+        return jdbcTemplate.queryForList(String.format(sql, tableName, yymmdd, endYYMMDD), String.class);
+        }catch(Exception ex){
+            throw new SQLException(ex);
+        }
     }
 
     /**
